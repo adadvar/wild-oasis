5f2594f7ff1afea7fe3e55eb1fc478dceb55fb39
adadvar <a.dadvar_71@yahoo.com>
2024-08-08 07:53:34 +0330

    fetching settings

---
+++ src/features/settings/UpdateSettingsForm.tsx
@@ -0,0 +1,39 @@
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import Spinner from "../../ui/Spinner";
+import { useSettings } from "./useSetting";
+
+function UpdateSettingsForm() {
+	const {
+		isLoading,
+		settings: {
+			minBookingLength,
+			maxBookingLength,
+			maxGuestsPerBooking,
+			breakfastPrice,
+		} = {},
+	} = useSettings();
+
+	if (isLoading) return <Spinner />;
+
+	return (
+		//@ts-ignore
+		<Form>
+			<FormRow label="Minimum nights/booking">
+				<Input type="number" id="min-nights" defaultValue={minBookingLength} />
+			</FormRow>
+			<FormRow label="Maximum nights/booking">
+				<Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+			</FormRow>
+			<FormRow label="Maximum guests/booking">
+				<Input type="number" id="max-guests" defaultValue={maxGuestsPerBooking} />
+			</FormRow>
+			<FormRow label="Breakfast price">
+				<Input type="number" id="breakfast-price" defaultValue={breakfastPrice} />
+			</FormRow>
+		</Form>
+	);
+}
+
+export default UpdateSettingsForm;
