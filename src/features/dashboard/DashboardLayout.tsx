b9760023296abee72fe975579de4d8e4b1b73cda
adadvar <a.dadvar_71@yahoo.com>
2024-08-28 06:55:28 +0330

    compute recent bookings & stats

---
+++ src/features/dashboard/DashboardLayout.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+
+const StyledDashboardLayout = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-template-rows: auto 34rem auto;
+	gap: 2.4rem;
+`;
+
+function DashboardLayout() {
+	const { isLoading: isLoading1, bookings } = useRecentBookings();
+	const { isLoading: isLoading2, stays } = useRecentStays();
+
+	if (isLoading1 || isLoading2) return <Spinner />;
+
+	return (
+		<StyledDashboardLayout>
+			<div>Statistics</div>
+			<div>Today's activity</div>
+			<div>Chart stay durations</div>
+			<div>Chart sales</div>
+		</StyledDashboardLayout>
+	);
+}
+
+export default DashboardLayout;
